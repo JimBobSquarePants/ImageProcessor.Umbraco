174de9139570b0bb38fb1ef30771ba467147a51d
leekelleher <leekelleher@gmail.com>
2014-01-07 10:41:21 +0000

    Bumped the version number.
    Took it out of 'beta'

--- src/ImageProcessor.Umbraco/Properties/VersionInfo.cs
+++ src/ImageProcessor.Umbraco/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.21.27275")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta")]
+[assembly: AssemblyFileVersion("1.0.21.29166")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
 
 
